f5e290bf2ef1cbd16fc964422d1219884a0c5c9e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:31:18 +0000

    Update container

--- src/Components/Navigation/NavContainer.tsx
+++ src/Components/Navigation/NavContainer.tsx
@@ -1,7 +1,12 @@
-export default function NavContainer(){
-    return(
-        <div>
-            Nav container
-        </div>
+import { Box, Flex } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+export default function NavContainer({ children }: { children: ReactNode }) {
+    return (
+        <Box>
+            <Flex padding={7}>
+                {children}
+            </Flex>
+        </Box>
     )
 }
