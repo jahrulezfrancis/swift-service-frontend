29f94c0160ece7f88b9c67e8e15c3eed11ac8b9f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:32:03 +0000

    Changed link tag to react-router

--- src/Components/Navigation/NavItemsRenderers.tsx
+++ src/Components/Navigation/NavItemsRenderers.tsx
@@ -1,10 +1,12 @@
 import { Navlinks } from "./navlinks";
+import { Link } from "react-router-dom";
+import { Stack } from "@chakra-ui/react";
 
 export default function NavItemsRenderers({ direction }: { direction: 'row' | 'column' | undefined }) {
     return (
         <Stack direction={direction} gap={5}>
             {Navlinks.map((item) => (
-                <Link textDecoration='none' target={item.title === "Github" ? "_blank" : ""} fontSize={20} key={item.id} href={item.url}>
+                <Link target={item.title === "Github" ? "_blank" : ""} key={item.id} to={item.url}>
                     {item.title}
                 </Link>
             ))}
