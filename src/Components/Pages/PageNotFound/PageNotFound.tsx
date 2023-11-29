d8f726e24e7161f7d609cbed738a475a8a1264ea
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 20:24:53 +0000

    Initial commit

---
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function PageNotFound() {
+    return (
+        <Box>
+            I am not so sure about the page you are looking but it seems to not be on our site
+        </Box>
+    )
+}
