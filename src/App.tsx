c72f2fef3003d5013a4bc497768370f52718349b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 20:25:22 +0000

    Adding application route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./Components/Pages/Home/HomePage"
+import SignIn from "./Components/Pages/Auth/Login"
+import SignUp from "./Components/Pages/Auth/SignUp"
+import PageNotFound from "./Components/Pages/PageNotFound/PageNotFound"
+
 function App() {
 
   return (
-    <>
-      <h1>Welcome to swift service</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="login" element={<SignIn />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   )
 }
 
