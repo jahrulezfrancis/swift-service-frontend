971d6ff2a6a9e78c809ec7bed7984aa8a2b5b10b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:27:54 +0000

    Updated Desktop nav bar

--- src/Components/Navigation/DesktopNav.tsx
+++ src/Components/Navigation/DesktopNav.tsx
@@ -1,7 +1,18 @@
+import { Box, Spacer } from "@chakra-ui/react";
+import NavContainer from "./NavContainer";
+import { Link } from "react-router-dom";
+import NavItemsRenderers from "./NavItemsRenderers";
+
 export default function DesktopNav() {
     return (
-        <div>
-            This is your DesktopNav
-        </div>
+        <Box>
+            <NavContainer children={
+                <>
+                    <Link to="/">Swift Service</Link>
+                    <Spacer />
+                    <NavItemsRenderers direction="row" />
+                </>
+            } />
+        </Box>
     )
 }
