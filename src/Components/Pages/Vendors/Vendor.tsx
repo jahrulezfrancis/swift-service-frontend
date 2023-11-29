fcbf5bdf5123947fef34be6931bd392c117e29c5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:26:43 +0000

    Initial commit

---
+++ src/Components/Pages/Vendors/Vendor.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function VendorPage() {
+    return (
+        <Box>
+            Your vendor page you know what I am saying
+        </Box>
+    )
+}
