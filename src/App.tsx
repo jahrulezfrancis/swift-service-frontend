71ccbda800a8fa15c451fb036a23ab0761a0acc3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:27:00 +0000

    Updating application route

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import Homepage from "./Components/Pages/Home/HomePage"
 import SignIn from "./Components/Pages/Auth/Login"
 import SignUp from "./Components/Pages/Auth/SignUp"
 import PageNotFound from "./Components/Pages/PageNotFound/PageNotFound"
+import { Box } from "@chakra-ui/react"
+import NavIndex from "./Components/Navigation/Index"
+import VendorPage from "./Components/Pages/Vendors/Vendor"
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="login" element={<SignIn />} />
-      <Route path="sign-up" element={<SignUp />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <Box>
+      <NavIndex />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="login" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="vendors" element={<VendorPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Box>
   )
 }
 
