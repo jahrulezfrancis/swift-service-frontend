74e6a158be41d7d44597f16536b9bfe74f6b5d30
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:28:22 +0000

    Added media query for mobile device

--- src/Components/Navigation/Index.tsx
+++ src/Components/Navigation/Index.tsx
@@ -1,8 +1,12 @@
+import { useMediaQuery } from "@chakra-ui/react"
 import DesktopNav from "./DesktopNav"
 import MobileNav from "./MobileNav"
 
+
+
 export default function NavIndex() {
-    const isMobileDevice = true
+    const [isMobileDevice] = useMediaQuery('(max-width: 624px)')
+
     return (
         <div>
             {isMobileDevice ? <MobileNav /> : <DesktopNav />}
