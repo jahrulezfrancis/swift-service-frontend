a13a35540b0611c41faa000fe0fac78f1d8d92cf
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:32:42 +0000

    Updating links path

--- src/Components/Navigation/navlinks.ts
+++ src/Components/Navigation/navlinks.ts
@@ -1,7 +1,7 @@
 export const Navlinks = [
     { id: 1, title: 'Home', url: '/' },
     { id: 2, title: 'About', url: '/about' },
-    { id: 3, title: 'Vendors', url: '/services' },
+    { id: 3, title: 'Vendors', url: '/vendors' },
     { id: 4, title: 'Sign Up', url: '/sign-in' },
     { id: 5, title: 'Login', url: '/sign-up' }
 ];
