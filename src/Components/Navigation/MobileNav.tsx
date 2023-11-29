6d58392975fc999c6d56a1bbfa1877fdb164c755
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-29 23:29:36 +0000

    Updated navigation items

--- src/Components/Navigation/MobileNav.tsx
+++ src/Components/Navigation/MobileNav.tsx
@@ -1,7 +1,18 @@
-export default function MobileNav(){
-    return(
-        <div>
-            This is your MobileNav
-        </div>
+import { Box, Spacer } from "@chakra-ui/react";
+import NavContainer from "./NavContainer";
+import { Link } from "react-router-dom";
+import NavItemsRenderers from "./NavItemsRenderers";
+
+export default function MobileNav() {
+    return (
+        <Box>
+            <NavContainer children={
+                <Box>
+                    <Link to="/">Swift Service</Link>
+                    <Spacer />
+                    <NavItemsRenderers direction="column" />
+                </Box>
+            } />
+        </Box>
     )
 }
