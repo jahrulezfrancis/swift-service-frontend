75efaf62a89d7bf74a5c69fcd4e6c1d22b35542c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-30 00:12:08 +0000

    Initial commit

---
+++ src/Components/Sections/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Footer(){
+    return(
+        <Box height={200}  bg="greenyellow">
+            <footer>
+                
+            </footer>
+        </Box>
+    )
+}
