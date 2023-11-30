4a4476de77622481662df8aed6fd94f15d8fcd5a
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-11-30 00:13:16 +0000

    Updating card design and layout

--- src/Components/Reuseables/Cards/ServiceCard.tsx
+++ src/Components/Reuseables/Cards/ServiceCard.tsx
@@ -1,7 +1,16 @@
+import { Box, Stack, HStack, Image, Heading, Text } from "@chakra-ui/react";
+import FoodImage from "../../../assets/raspberries.jpg"
+
 export default function ServiceCard() {
     return (
-        <div>
-            Service
-        </div>
+        <Box minW={600} padding={10} minH="300px" boxShadow="5px 1px 30px -14px rgba(58,49,49,0.75)">
+            <HStack gap={10}>
+                <Image height="300px" objectFit="contain" width="350px" src={FoodImage} />
+                <Stack>
+                    <Heading fontSize={22}>Your text goes here you know what I am saying</Heading>
+                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis culpa natus, magni numquam nulla commodi quaerat fugiat quam enim ipsa molestias nesciunt voluptatum iure recusandae, nemo veritatis excepturi quia. Aut?</Text>
+                </Stack>
+            </HStack>
+        </Box>
     )
 }
