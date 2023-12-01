d7777658c5866497da414a088829a8425d422056
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-01 02:25:54 +0000

    Implementing slider animation

--- src/Components/Sections/Slider/HeroSlider.tsx
+++ src/Components/Sections/Slider/HeroSlider.tsx
@@ -1,9 +1,37 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import './styles.css';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function HeroSlider() {
-    return (
-        <Stack align="center" justify="center" bg='blueviolet' h='500px'>
-            <Heading>This is the hero slider</Heading>
-        </Stack>
-    )
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide >Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
