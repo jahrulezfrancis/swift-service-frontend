2c5e7819b77e5f6db297d3f2818c890c55734119
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-01 11:51:58 +0000

    Initial commit

---
+++ src/Components/Forms/Signup/RegistrationForm.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function RegistrationForm() {
+    return (
+        <Box>
+            <Heading>Sign Up form</Heading>
+            You can sign up for a new account here
+        </Box>
+    )
+}
