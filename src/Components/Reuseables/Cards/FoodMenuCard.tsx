015cfed4eef9318ae381df8ad2e90f90a43947c1
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-01 00:36:14 +0000

    Updated Restaurants card styling and added content

--- src/Components/Reuseables/Cards/FoodMenuCard.tsx
+++ src/Components/Reuseables/Cards/FoodMenuCard.tsx
@@ -1,7 +1,21 @@
-export default function FoodMenuCard() {
+import { Button, Stack, Image, Spacer } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+interface RestuarantsCardProps {
+    imageUrl: string;
+    menuLink: string
+}
+
+export default function RestaurantsCard(props: RestuarantsCardProps) {
     return (
-        <div>
-            Food menu card
-        </div>
+        <Stack justify='center' align="center" w="420px">
+            <Image width="416px" alt="" src={props.imageUrl} />
+            <Stack borderRadius={8} mt={-5} justify="center" align="center" width="90%" bg="white" padding="20px 0px">
+                <Link to={props.menuLink}>
+                    <Button _hover={{ color: "white", bg: "#FFA386"}} color="white" bg="#FF5722" height="60px" width={"200px"}>Browse Menu</Button>
+                </Link>
+            </Stack>
+            <Spacer height={20} />
+        </Stack>
     )
 }
