37bb475da4dd14a47856dd25ab6afdf6a4fb64b5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-01 00:35:20 +0000

    Adding homepage content

--- src/Components/Pages/Home/HomePage.tsx
+++ src/Components/Pages/Home/HomePage.tsx
@@ -1,18 +1,97 @@
-import { Box, Heading, Stack } from "@chakra-ui/react";
+import { Box, Heading, Stack, Flex, Tabs, TabList, Tab, HStack, TabPanel, TabPanels, Accordion, AccordionIcon, AccordionButton, AccordionItem, AccordionPanel, Spacer, Text, Icon, } from "@chakra-ui/react";
 import HeroSlider from "../../Sections/Slider/HeroSlider";
 import ServiceCard from "../../Reuseables/Cards/ServiceCard";
+import RestaurantsCard from "../../Reuseables/Cards/FoodMenuCard";
+import { BsCartPlusFill } from "react-icons/bs";
+import ListItemComp from "../../Sections/listItems";
+import DemoPic from "../../../assets/raspberries.jpg"
+
 
 export default function Homepage() {
     return (
-        <Box>
-            <HeroSlider />
-            <section>
-                <Heading padding="50px 0px" textAlign="center" color="#FFB03B">Services</Heading>
-                <Stack margin="50px 20px" gap={10}>
-                    <ServiceCard />
-                    <ServiceCard />
-                </Stack>
-            </section>
+        <Box backgroundImage={DemoPic}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundAttachment="fixed"
+            minW="98vw"
+            minHeight="100vh"
+        >
+            <Box backgroundColor="rgba(255, 255, 255, 0.8)">
+                <HeroSlider />
+                <section style={{ background: "white" }}>
+                    <Box bg="white">
+                        <Heading padding="50px 0px" textAlign="center" color="#FFB03B">Services</Heading>
+                        <Stack margin="50px 20px" gap={10}>
+                            <ServiceCard />
+                            <ServiceCard />
+                        </Stack>
+                    </Box>
+                </section>
+                <section style={{ background: "white", marginTop: "-50px" }}>
+                    <Box p="50px 0px 20px 0px" bg="white">
+                        <Flex align="center" justify='center'>
+                            <Accordion borderRadius={8} border="1px #CECECE solid" w="85vw" defaultIndex={[0]} allowMultiple>
+                                <AccordionItem>
+                                    <h2>
+                                        <AccordionButton>
+                                            <Box flex='1' textAlign='center'>
+                                                <Heading textAlign="center" fontSize={30}>Request Service</Heading>
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                    </h2>
+                                    <AccordionPanel pb={4}>
+                                        <Tabs>
+                                            <TabList>
+                                                <Tab>Pickup & delivery</Tab>
+                                                <Tab>Cleaning</Tab>
+                                                <Tab>Task Delegation</Tab>
+                                            </TabList>
+                                            <TabPanels>
+                                                <TabPanel>
+                                                    Pickup and delivery form goes here
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <p>Coming Soon!</p>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <p>Coming Soon!</p>
+                                                </TabPanel>
+                                            </TabPanels>
+                                        </Tabs>
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            </Accordion>
+                        </Flex>
+                    </Box>
+                </section>
+                <Spacer bg="white" height={100} />
+
+                <section>
+                    <Box padding="50px 0px">
+                        <Box>
+                            <Heading textAlign="center">Partnered Restaurants</Heading>
+                            <Text textAlign="center">Browse and order spetacular dishes from certified Restaurants</Text>
+                        </Box>
+                        <HStack p={10} gap={10} justify="center">
+                            <RestaurantsCard imageUrl="/src/assets/raspberries.jpg" menuLink="/market" />
+                            <RestaurantsCard imageUrl="/src/assets/raspberries.jpg" menuLink="/market" />
+                            <RestaurantsCard imageUrl="/src/assets/raspberries.jpg" menuLink="/market" />
+                        </HStack>
+                    </Box>
+                </section>
+
+                <section>
+                    <Box p="20px 0px" bg="white">
+                        <HStack align="start" justify='space-evenly' wrap="wrap">
+                            <ListItemComp icon={<Icon as={BsCartPlusFill} />} lineOne="No. 7, yelwa Bauchi" heading="Location" />
+                            <ListItemComp icon={<Icon as={BsCartPlusFill} />} lineOne="Saturday - Sunday" lineTwo="6:00 AM - 6:00 PM" heading="Open Hours" />
+                            <ListItemComp icon={<Icon as={BsCartPlusFill} />} lineOne="help@swiftservice.com - Sunday" lineTwo="contact@swiftservice.com" heading="Email" />
+                            <ListItemComp icon={<Icon as={BsCartPlusFill} />} lineOne="+2349024551324" lineTwo="+2348067884638" heading="Call" />
+                        </HStack>
+                    </Box>
+                </section>
+            </Box>
         </Box>
     )
 }
